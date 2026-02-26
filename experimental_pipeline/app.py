b64b63fa63461cb4e7594266d79c958d5c11e061
Muhammad Ali <141209093+aliawan4027@users.noreply.github.com>
2026-02-26 12:04:58 +0500

    Update app.py

--- experimental_pipeline/app.py
+++ experimental_pipeline/app.py
@@ -6,9 +6,9 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"status": "API is running"}
+    return {"status":"API is running"}
 
 @app.post("/run-experiment")
 def run():
     run_experiment()
-    return {"status": "Experiment executed and CSV saved"}
+    return {"status":"Experiment executed and CSV saved"}
